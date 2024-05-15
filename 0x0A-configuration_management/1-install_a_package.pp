3a96fc34ac6eab947d0fe05cd1b45dea33313a52
0obito <elmsafer.abdellah00@gmail.com>
2024-05-15 16:48:51 +0100

    siiike

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/pup
 # This puppet code installs Flask version 2.1.0 from pip3
-package {'installflask':
+package {'flask':
   name     => 'flask',
   ensure   => '2.1.0',
   provider => 'pip3',
