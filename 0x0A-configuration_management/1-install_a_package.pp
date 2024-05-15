7e927f54c8c55da1117eff0c2077f0083efee07d
0obito <elmsafer.abdellah00@gmail.com>
2024-05-15 14:09:00 +0100

    more insisting

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,5 @@
-# This puppet code installs flask from pip3
-package { 'flask 2.1.0':
-provider => 'pip3'
+# This puppet code installs Flask version 2.1.0 from pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
