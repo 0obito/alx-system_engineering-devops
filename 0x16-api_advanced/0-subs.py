63cdb5c1b4819993410f45d839b77bad7f255338
0obito <elmsafer.abdellah00@gmail.com>
2024-06-04 21:01:48 +0100

    tried new approach

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,14 +7,18 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    returns the total number of subscribers in a given subreddit
+    Returns the total number of subscribers in a given subreddit.
+    If the subreddit is invalid, returns 0.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'API Practice'}
+    headers = {'User-Agent': 'RedditSubscriberCountBot/0.1 by YourUsername'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
         return 0
-    else:
+
+    try:
         data = response.json()
         return data['data']['subscribers']
+    except (KeyError, ValueError):
+        return 0
