fd9403b10d4ace6402ead62255a5d615e0144e44
0obito <elmsafer.abdellah00@gmail.com>
2024-06-04 21:07:00 +0100

    if __name__ is __main__

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,20 +5,17 @@ this is a 0-subs.py module
 import requests
 
 
-def number_of_subscribers(subreddit):
-    """
-    Returns the total number of subscribers in a given subreddit.
-    If the subreddit is invalid, returns 0.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'RedditSubscriberCountBot/0.1 by YourUsername'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+if __name__ == "__main__":
+    def number_of_subscribers(subreddit):
+        """
+        returns the total number of subscribers in a given subreddit
+        """
+        url = f"https://www.reddit.com/r/{subreddit}/about.json"
+        headers = {'User-Agent': 'API Practice'}
+        response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code != 200:
-        return 0
-
-    try:
-        data = response.json()
-        return data['data']['subscribers']
-    except (KeyError, ValueError):
-        return 0
+        if response.status_code != 200:
+            return 0
+        else:
+            data = response.json()
+            return data['data']['subscribers']
