b8a20d2393074ce046fda4c76bf57fe2218428a0
0obito <elmsafer.abdellah00@gmail.com>
2024-06-05 01:30:25 +0100

    1 with new approach as well

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,20 +1,37 @@
 #!/usr/bin/python3
 """this is a module for 1-top_ten.py"""
 
+import requests
+
 
 def top_ten(subreddit):
     """
     returns the top 10 hot posts
     of the subreddit
     """
-    import requests
+    base_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
-        print('None')
-    else:
-        [print(child.get("data").get("title"))
-         for child in sub_info.json().get("data").get("children")]
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
+    params = {'limit': 10}
+
+    response = requests.get(base_url, headers=headers, params=params)
+
+    if response.status_code != 200:
+        print(None)
+        return
+
+    data = response.json()
+
+    if 'data' not in data:
+        print(None)
+        return
+
+    posts = data['data']['children']
+
+    if not posts:
+        print(None)
+        return
+
+    for post in posts:
+        print(post['data']['title'])
