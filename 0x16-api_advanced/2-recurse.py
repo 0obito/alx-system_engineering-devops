cf9348ca570197668b444dc09219ecc987bf5fe0
0obito <elmsafer.abdellah00@gmail.com>
2024-06-05 01:25:49 +0100

    2 with a new approach

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,29 +3,35 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], after="", count=0):
-    """ Returns a list of titles of all hot posts on a given subreddit """
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)"
-    }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
+def recurse(subreddit, hot_list=[], after=None):
+    """
+    returns a list containing the titles
+    of all hot articles for a given subreddit
+    """
+    base_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
+    params = {'after': after} if after else {}
+
+    response = requests.get(base_url, headers=headers, params=params)
+
+    if response.status_code != 200:
         return None
 
-    results = response.json().get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        hot_list.append(c.get("data").get("title"))
+    data = response.json()
 
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
+    if 'data' not in data:
+        return None
+
+    posts = data['data']['children']
+
+    for post in posts:
+        hot_list.append(post['data']['title'])
+
+    after = data['data']['after']
+
+    if after:
+        return recurse(subreddit, hot_list, after)
+
     return hot_list
